f61ab88f8f0609d6f8466f63e8e8618336982a07
tpokora <pokora.tomek@gmail.com>
2017-06-25 20:02:58 +0200

    Exercise application PORT settings

--- core/src/main/java/com/tpokora/exercises/ExercisesApplication.java
+++ core/src/main/java/com/tpokora/exercises/ExercisesApplication.java
@@ -12,5 +12,13 @@ public class ExercisesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExercisesApplication.class, args);
+		String webPort = System.getenv("PORT");
+		if (webPort == null || webPort.isEmpty()) {
+			webPort = "7080";
+		}
+
+		System.setProperty("server.port", webPort);
+		SpringApplication.run(ExercisesApplication.class, args);
+
 	}
 }
